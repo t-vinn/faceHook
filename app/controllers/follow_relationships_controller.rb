dde3baec1a9d7bf5d746879df931358c914e9ab0
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-03 16:49:40 +0900

    suspended, first making amends for devise. currently having trouble with followee.user_id

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -2,4 +2,20 @@ class FollowRelationshipsController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @follow_relationship = FollowRelationship.new
+  end
+
+  def create
+    @follow_relationship = FollowRelationship.new(params.require(:follow_relationship).permit(:follower_user_id, :followee_user_id))
+
+    if @follow_relationship.save
+      redirect_to user_follow_relationships_path(current_user.id)
+      flash.now[:alert] = 'Followed new person!'
+    else
+      redirect_to user_follow_relationships_path(current_user.id)
+      flash.now[:alert] = 'follow failed!'
+    end
+  end
 end
