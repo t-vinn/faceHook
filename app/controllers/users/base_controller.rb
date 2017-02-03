c346ac3fed9fddb26889132b27167929cec6fb96
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-03 20:08:35 +0900

    namespace user

---
+++ app/controllers/users/base_controller.rb
@@ -0,0 +1,14 @@
+class Users::BaseController < ApplicationController
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birth_date])
+  end
+
+end
+
