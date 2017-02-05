890eee6533b17477bbafaede901dec590c27db0b
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-05 20:55:50 +0900

    modified follow_relationships_controller

--- app/controllers/users/follow_relationships_controller.rb
+++ app/controllers/users/follow_relationships_controller.rb
@@ -1,4 +1,5 @@
 class Users::FollowRelationshipsController < Users::BaseController
+
   def index
     @users = User.all
   end
@@ -14,14 +15,14 @@ class Users::FollowRelationshipsController < Users::BaseController
     #@created = Time.now
     #@follow_relationship = FollowRelationship.create(id: @id, follower_user_id: current_user.id, followee_user_id: @followee_id, created_at: @created, updated_at: @created)
     @follow_relationship = FollowRelationship.new(params.require(:follow_relationship).permit(:follower_user_id, :followee_user_id))
+    @follow_relationship.user = current_user
 
 
 
-    if @follow_relationship.save
+    if @follow_relationship.save!
       redirect_to users_user_follow_relationships_path(current_user.id)
       flash.now[:alert] = 'Followed new person!'
     else
-      redirect_to users_user_follow_relationships_path(current_user.id)
       flash.now[:alert] = 'follow failed!'
     end
   end
