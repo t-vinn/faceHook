8150136237a9f7417e1a4ff3ba72f4fd3109f708
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-05 17:41:22 +0900

    registrations_controller made easier to read

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :password, :password_confirmation])
      super
   end
+  
   def account_update_params
      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :password, :password_confirmation, :current_password, :birth_date])
      super
