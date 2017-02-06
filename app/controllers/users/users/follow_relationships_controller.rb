4e2ebc863cc0dda77be91ded564293e239f8762e
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-06 21:15:47 +0900

    showing links to 'follow' only with unfollowing users, links to 'unfollow' yet to be done

--- app/controllers/users/users/follow_relationships_controller.rb
+++ app/controllers/users/users/follow_relationships_controller.rb
@@ -1,17 +1,6 @@
 class Users::Users::FollowRelationshipsController < Users::BaseController
 
   def index
-    @active_relationships = FollowRelationship.where(:follower_user_id => current_user.id)
-
-    @unfollowing_users = User.all
-    @following_users = current_user.following_users
-    @unfollowing_users.delete(current_user) #これだとUser消えちゃう
-    @unfollowing_users.except(@following_users) #これだとUser消えちゃう
-    #@following_users.each do |following_user|
-      #@unfollowing_users.delete(following_user)
-    #end
-
-
   end
 
   def new
@@ -27,7 +16,7 @@ class Users::Users::FollowRelationshipsController < Users::BaseController
     @follow_relationship = FollowRelationship.new(params.require(:follow_relationship).permit(:follower_user_id, :followee_user_id))
 
     if @follow_relationship.save!
-      redirect_to users_user_follow_relationships_path(current_user.id)
+      redirect_to users_follow_relationships_path(current_user.id)
       flash.now[:alert] = 'Followed new person!'
     else
       flash.now[:alert] = 'follow failed!'
