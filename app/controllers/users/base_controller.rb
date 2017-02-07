60082175a130b699fcde860b7cfbd63bf4ab4b04
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-07 15:48:37 +0900

    existing files except those in branch 'follow' modified to meet rubocop demands.

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -1,14 +1,14 @@
-class Users::BaseController < ApplicationController
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
+module Users
+  class BaseController < ApplicationController
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protect_from_forgery with: :exception
 
-  protected
+    protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birth_date])
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birth_date])
+    end
   end
-
 end
-
