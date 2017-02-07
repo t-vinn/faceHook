a83c467e7ac616a6cd18b8867a34ce6fd3ce5ea2
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-07 18:25:32 +0900

    small modifications to indentations

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -5,7 +5,8 @@ module Users
       @unfollowing_users = User.all - @following_users
       @unfollowing_users.delete(current_user)
 
-      @follow_relationships_index_by_followee_user_id = current_user.active_relationships.index_by(&:followee_user_id)
+      @follow_relationships_index_by_followee_user_id = \
+        current_user.active_relationships.index_by(&:followee_user_id)
     end
   end
 end
