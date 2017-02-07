646b95418e4eff89d3fb1dc21d9f09fac8789616
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-07 18:17:59 +0900

    query in view moved to controller & garbage links destroyed

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -4,6 +4,8 @@ module Users
       @following_users = current_user.following_users
       @unfollowing_users = User.all - @following_users
       @unfollowing_users.delete(current_user)
+
+      @follow_relationships_index_by_followee_user_id = current_user.active_relationships.index_by(&:followee_user_id)
     end
   end
 end
