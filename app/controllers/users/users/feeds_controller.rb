dacfd70f6b8d6db62ab15eef0b6d41665f96f11b
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-10 12:14:48 +0900

    created initial view files

--- app/controllers/users/users/feeds_controller.rb
+++ app/controllers/users/users/feeds_controller.rb
@@ -1,6 +1,25 @@
 module Users
   module Users
-    class FollowRelationshipsController < BaseController
+    class FeedsController < BaseController
+      def index
+        @feed = Feed.new
+        @feeds = Feed.all
+      end
+
+      def create
+        @feed = Feed.new(feed_params)
+        if @feed.save
+          redirect_to '/', notice: 'a new feed created'
+        else
+          flash.now[:alert] = 'the message is too short'
+          render :new
+        end
+      end
+
+      private
+        def feed_params
+          params.require(:feed).permit(:user_id, :content, :privacy)
+        end
     end
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,5 @@
 class Feed < ApplicationRecord
-  enum privacy: { public: 0, follower_only: 1, private: 2 }
+  enum privacy: { public_feed: 0, follower_only_feed: 1, private_feed: 2 }
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true
