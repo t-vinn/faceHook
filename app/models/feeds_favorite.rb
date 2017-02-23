74ecc53c48806aeeaf72ab924c43b48e6fedc10f
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-23 20:20:37 +0900

    rubocop: trailing whitespace deleted

--- app/models/feeds_favorite.rb
+++ app/models/feeds_favorite.rb
@@ -2,5 +2,5 @@ class FeedsFavorite < ApplicationRecord
   belongs_to :user
   belongs_to :feed
   validates :user_id, presence: true
-  validates :feed_id, presence: true 
+  validates :feed_id, presence: true
 end
