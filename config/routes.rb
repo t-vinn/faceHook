636ba86673092c67ced272e5d2eefa6846fb854e
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-23 14:34:28 +0900

    routes.rb revised

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   root 'users/users#index'
   namespace :users do
     resources :follow_relationships, only: [:create, :destroy], module: :users
-    resources :feeds, module: :users
+    resources :feeds, module: :users do
+      resources :replies, only: [:new, :create]
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
