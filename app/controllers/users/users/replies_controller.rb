c2755259af7c553be2256d57e2697094fff70548
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-24 15:52:29 +0900

    before_action deleted

--- app/controllers/users/users/replies_controller.rb
+++ app/controllers/users/users/replies_controller.rb
@@ -1,9 +1,8 @@
 module Users
   module Users
     class RepliesController < BaseController
-      before_action :set_feed, only: [:new]
-
       def new
+        @feed = Feed.find(params[:feed_id])
         @reply = Reply.new
       end
 
@@ -16,10 +15,6 @@ module Users
       end
 
       private
-
-        def set_feed
-          @feed = Feed.find(params[:feed_id])
-        end
 
         def reply_params
           params.require(:reply).permit(:user_id, :content, :feed_id)
