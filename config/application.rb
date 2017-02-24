d477a9f2c4740541eea08636cbef2d398e1deae0
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-25 08:06:19 +0900

    load secrets.yml to avoid inserting 'export DB_PASSWORD' before rails s

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV.update YAML.load_file('config/secrets.yml')[Rails.env] rescue {}
+
 module Facebook
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
