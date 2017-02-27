5a344db7fae49bcff52718c46148dc790d7cd1ba
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-27 20:39:39 +0900

    rubocop

--- app/controllers/users/users/group_post_favorites_controller.rb
+++ app/controllers/users/users/group_post_favorites_controller.rb
@@ -3,7 +3,9 @@ module Users
     class GroupPostFavoritesController < BaseController
       def create
         group = Group.find(params[:group_id])
-        group_post_favorite = current_user.group_post_favorites.build(group_post_id: params[:group_post_id])
+        group_post_favorite = current_user.group_post_favorites.build(
+          group_post_id: params[:group_post_id]
+        )
         if group_post_favorite.save
           redirect_to users_group_path(group), notice: 'You liked a post!'
         else
@@ -19,7 +21,6 @@ module Users
           flash.now[:notice] = 'An error occured and faceHook failed to delete your favorite.'
         end
       end
-
     end
   end
 end
