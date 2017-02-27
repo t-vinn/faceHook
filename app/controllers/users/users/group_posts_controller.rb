e9a093acbd885660febe5f473c1080ba1172910a
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-02-27 20:01:10 +0900

    rubocop

--- app/controllers/users/users/group_posts_controller.rb
+++ app/controllers/users/users/group_posts_controller.rb
@@ -3,16 +3,16 @@ module Users
     class GroupPostsController < BaseController
       def create
         group = Group.find(params[:group_id])
-       group_post = group.group_posts.new(group_post_params) 
-       if group_post.save
-         redirect_to users_group_path(group), notice: 'New group post!'
-       else
-         redirect_to users_group_path(group), notice: 'Failed. Try again.'
-       end
+        group_post = group.group_posts.new(group_post_params)
+        if group_post.save
+          redirect_to users_group_path(group), notice: 'New group post!'
+        else
+          redirect_to users_group_path(group), notice: 'Failed. Try again.'
+        end
       end
 
       private
-      
+
         def group_post_params
           params.require(:group_post).permit(:user_id, :group_id, :content)
         end
