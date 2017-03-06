ee71acbb13a1da9e34e082c9daf8b9830c99b1e8
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-06 20:53:30 +0900

    rubocop

--- app/admin/feed.rb
+++ app/admin/feed.rb
@@ -1,4 +1,3 @@
-# rubocop: disable Metrics/BlockLength
 ActiveAdmin.register Feed do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
