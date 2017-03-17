b27ec55c846ecfd21da379a354dc5fee412fe14c
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-17 16:43:46 +0900

    groups_controller_spec.rb

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -5,18 +5,25 @@ RSpec.describe Users::GroupsController, type: :controller do
 
   describe 'GET #index' do
     it 'assigns the requested group to @group' do
-
+      get :index
+      expect(assigns(:group)).to be_a_new @group
     end
 
     it 'assigns the requested groups to @groups' do
-
+      school = create(:group) 
+      office = create(:group)
+      get :index
+      expect(assigns(:groups)).to match_array([school, office])
     end
 
     it 'renders the :index template' do
-
+      get :index
+      expect(response).to render_template :index
     end
 
     it 'returns 200' do
+      get :index
       expect(response.status).to eq 200
     end
   end
+end
