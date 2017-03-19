f26ea85be2698cbbe2dfdda9c251216c55cab486
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-19 13:49:41 +0900

    modify sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Users::SessionsController, type: :controller do
   describe 'GET #new' do
-    it 'returns 200' do
+    before do
       @request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+    it 'returns 200' do
       get :new
       expect(response.status).to eq 200
     end
 
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+
     it 'assigns the requested feeds to @public_feeds' do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
       @feeds = create_list(:feed, 2)
       get :new
       expect(assigns(:public_feeds)).to eq @feeds
