eca4b184d9af1dfb9a97ff85a2ff6885efb43d1f
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-20 20:49:09 +0900

    trying to use subject in 2 ways

--- spec/controllers/feeds/feed_favorites_controller_spec.rb
+++ spec/controllers/feeds/feed_favorites_controller_spec.rb
@@ -1,31 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Users::Feeds::FeedFavoritesController, type: :controller do
-  login_user
 
   describe 'POST #create' do
     context 'with valid parameters' do
-      let(:feed) { create(:feed) }
+      login_user
+      binding.pry
+      let(:feed) { create(:feed, :with_user) }
+      let(:feed_favorite) { subject.current_user.feed_favorites.build(feed: feed) }
+      let(:feed_favorite_params) {{ user_id: feed_favorite.user_id, feed_id: feed_favorite.feed_id }}
 
       it 'returns 302' do
-        post :create, params: { feed_id: feed, feed_favorite: attributes_for(:feed_favorite) }
-        expect(response.status).to eq 302
+        post :create, params: { feed_id: feed_favorite.feed, feed_favorite: feed_favorite_params } 
+        is_expected.to have_http_status(302)
       end
 
       it 'adds a feed_favorite to the database' do
-        expect{
-          post :create, params: { feed_id: feed, feed_favorite: attributes_for(:feed_favorite) }
-        }.to change(FeedFavorite, :count).by(1)
+        post :create, params: { feed_id: feed_favorite.feed, feed_favorite: feed_favorite_params } 
+        expect{ subject }.to change(FeedFavorite, :count).by(1)
       end
 
       it 'redirects to root path' do
-        post :create, params: { feed_id: feed, feed_favorite: attributes_for(:feed_favorite) }
-        expect(response).to redirect_to root_path
+        post :create, params: { feed_id: feed_favorite.feed, feed_favorite: feed_favorite_params } 
+        is_expected.to redirect_to root_path
       end
 
     end
 
     context 'with invalid parameters' do
+      login_user
       let(:feed) { create(:feed) }
       
       it 'returns 302' do
@@ -48,6 +51,7 @@ RSpec.describe Users::Feeds::FeedFavoritesController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+    login_user
     let!(:feed_favorite) { create(:feed_favorite, :with_feed, :with_user) }
 
     it 'deletes the feed_favorite' do
