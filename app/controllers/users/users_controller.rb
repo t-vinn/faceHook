c6b98150cfb5c126daf51363aec2f93a8c8e369b
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-22 10:13:53 +0900

    array can't be ordered

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -17,14 +17,14 @@ module Users
       group_posts = GroupPost.where(group_id: groups.pluck(:id)) \
                              .includes(:group, :user, :group_post_pictures)
       feeds_or_group_posts = \
-        (feeds | group_posts).order('created_at DESC')
+        (feeds | group_posts).sort_by(&:created_at).reverse
       @posts = Kaminari.paginate_array(feeds_or_group_posts).page(params[:page])
       following_feeds = Feed.where(
         privacy: [:share_with_all, :share_with_follower],
         user: current_user.following_users
       ).includes(:user, [replies: :user])
       following_feeds_or_group_posts = \
-        (following_feeds | group_posts).order('created_at DESC')
+        (following_feeds | group_posts).sort_by(&:created_at).reverse
       @following_posts = Kaminari.paginate_array(following_feeds_or_group_posts).page(params[:page])
       @feed_favorites_index_by_feed_id = current_user.feed_favorites.index_by(&:feed_id)
       @reply_favorites_index_by_reply_id = current_user.reply_favorites.index_by(&:reply_id)
