8c76a8e8c7592c59cb7681c5fe6892c02f509173
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-23 16:23:45 +0900

    similarity should not be duplicated

--- app/models/similarities_user.rb
+++ app/models/similarities_user.rb
@@ -1,5 +1,5 @@
 class SimilaritiesUser < ApplicationRecord
-  validates :similarity_id, presence: true
+  validates :similarity_id, presence: true, uniqueness: { scope: [:user_id] }
   validates :user_id, presence: true
   belongs_to :similarity
 end
