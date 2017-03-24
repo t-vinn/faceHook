aaf99b55169271ef025c105bee0e7abccb1bae47
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-24 16:02:57 +0900

    return back rescued

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_request_from
+
   rescue_from Exception, with: :render_500
 
   rescue_from ActionController::RoutingError, with: :render_404
@@ -10,5 +12,21 @@ class ApplicationController < ActionController::Base
 
   def render_500
     render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+  end
+
+  def set_request_from
+    if session[:request_from]
+      @request_from = session[:request_from]
+    end
+    # save which page the user last visited
+    session[:request_from] = request.original_url
+  end
+
+  def return_back
+    if request.referer
+      redirect_to :back and return true
+    elsif @request_from
+      redirect_to @request_from and return true
+    end
   end
 end
