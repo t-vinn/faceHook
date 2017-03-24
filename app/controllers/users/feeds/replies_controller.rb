1e8d1e4d580d596a50e5b1a0b20951878ac3b70f
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-24 16:13:49 +0900

    a single back only returns to edit page, which is unacceptable

--- app/controllers/users/feeds/replies_controller.rb
+++ app/controllers/users/feeds/replies_controller.rb
@@ -19,7 +19,7 @@ module Users
           reply = Reply.new(reply_params)
           if reply.save
             UserMailer.reply_creation(reply).deliver_later
-            redirect_to :back, notice: 'You successfully replied to a comment!'
+            redirect_to root_path, notice: 'You successfully replied to a comment!'
           else
             redirect_to new_users_feed_reply_path, notice: 'Your message is too short or long!'
           end
