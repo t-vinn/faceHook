cdc80353fa554176c2506f6fbdbdec562b9f48f5
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-26 17:38:11 +0900

    modify groups_controller_spec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,68 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Users::GroupsController, type: :controller do
+  login_user
   describe 'GET #index' do
-    login_user
+    let(:groups) { create_list(:group, 2, owner_user_id: controller.current_user.id) }
+    subject { get :index }
     it 'assigns the requested group to @group' do
-      get :index
+      subject
       expect(assigns(:group)).to be_a_new(Group)
     end
 
     it 'assigns the requested groups to @groups' do
-      groups = user.groups.create_list(:group, 2)
-      get :index
-      expect(assigns(:groups)).to match_array(groups)
+      create(:groups_user, group: groups[0], user: controller.current_user)
+      subject
+      expect(assigns(:groups)).to match_array([groups[0]])
     end
 
     it 'renders the :index template' do
-      get :index
-      expect(response).to render_template :index
+      is_expected.to render_template :index
     end
 
     it 'returns 200' do
-      get :index
-      expect(response.status).to eq 200
+      is_expected.to have_http_status 200
     end
   end
 
   describe 'GET #show' do
-    login_user
+    let(:group) { create(:group) }
+    let!(:group_post) { create(:group_post, group: group) }
+    let!(:group_post_favorite) { create(:group_post_favorite, user: controller.current_user, group_post: group_post) }
+    subject { get :show, params: { id: group } }
+
     it 'assigns the requested group to @group' do
-      @group = create(:group)
-      get :show, params: { id: @group }
-      expect(assigns(:group)).to eq @group
+      subject
+      expect(assigns(:group)).to eq group
     end
 
     it 'assigns the requested group_posts to @group_posts' do
-      @group = create(:group)
-      created_posts = create_list(:group_post, 5)
-      get :show, params: { id: @group }
-      expect(assigns(:group_posts)).to match_array(created_posts)
+      subject
+      expect(assigns(:group_posts)).to match_array([group_post])
     end
 
     it 'assigns the requested group_post to @group_post' do
-      @group = create(:group)
-      get :show, params: { id: @group }
+      subject
       expect(assigns(:group_post)).to be_a_new(GroupPost)
     end
 
     it 'assigns the requested group_post_favorites to @group_post_favorites_index_by_group_post_id' do
-      @group = create(:group)
-      group_post_favorites = create_list(:group_post_favorite, 5)
-      get :show, params: { id: @group }
-      expect(assigns(:group_post_favorites_index_by_group_post_id)).to match_array(group_post_favorites)
+      subject
+      expect(assigns(:group_post_favorites_index_by_group_post_id)).to match_array([[group_post_favorite.id, group_post_favorite]])
     end
 
     it 'renders the :show template' do
-      @group = create(:group)
-      get :show, params: { id: @group }
-      expect(response).to render_template :show
+      is_expected.to render_template :show
     end
 
     it 'returns 200' do
-      @group = create(:group)
-      get :show, params: { id: @group }
-      expect(response.status).to eq 200
+      is_expected.to have_http_status 200
     end
   end
 end
