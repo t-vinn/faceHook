bef0a9fec470bf91cf9f127f822f04ad35cb0823
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-27 13:07:12 +0900

    should redirect to groups#show, not root

--- app/controllers/users/groups/group_posts/group_post_favorites_controller.rb
+++ app/controllers/users/groups/group_posts/group_post_favorites_controller.rb
@@ -13,16 +13,17 @@ module Users
             )
             if group_post_favorite.save
               UserMailer.group_post_favorite_creation(group_post_favorite).deliver_later
-              redirect_to root_path, notice: 'You liked a post!'
+              redirect_to users_group_path(group), notice: 'You liked a post!'
             else
-              redirect_to root_path, notice: 'FAIL. Try again.'
+              redirect_to users_group_path(group), notice: 'FAIL. Try again.'
             end
           end
         end
 
         def destroy
+          group = Group.find(params[:group_id])
           if GroupPostFavorite.find(params[:id]).destroy
-            redirect_to root_path, notice: 'Your favorite deleted.'
+            redirect_to users_group_path(group), notice: 'Your favorite deleted.'
           else
             flash.now[:notice] = 'An error occured and faceHook failed to delete your favorite.'
           end
