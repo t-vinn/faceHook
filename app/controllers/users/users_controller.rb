3b19bccce98559412710f5d5987b59e40baac66a
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-27 11:45:46 +0900

    remove unnecessary commetn

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -3,7 +3,6 @@ module Users
   class UsersController < BaseController
     def index
       @following_users = current_user.following_users
-      # @unfollowing_users = User.where(id: current_user.recommended_user_ids)
       @unfollowing_users = current_user.recommended_user_ids.first(10).map { |id| User.find(id) }
       @follow_relationships_index_by_followee_user_id = \
         current_user.active_relationships.index_by(&:followee_user_id)
