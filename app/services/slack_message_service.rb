1f5c7c607457107256dc2c45a7d024062377d773
Toshihiro Nakayama <twg.btaiwc@gmail.com>
2017-03-27 15:03:40 +0900

    use only necessary attributes in instance variables

--- app/services/slack_message_service.rb
+++ app/services/slack_message_service.rb
@@ -1,12 +1,13 @@
 class SlackMessageService
   def initialize(post_with_content)
-    @post_with_content = post_with_content
+    @content = post_with_content.content
+    @class_name = post_with_content.class.name
   end
 
   def send_to_times
     url = ENV['URL_SLACK']
-    msg = @post_with_content.class.name + " was posted. The message was \n" + \
-          @post_with_content.content
+    msg = @class_name + " was posted. The message was \n" + \
+          @content
     HTTParty.delay.post(url, body: { "text": msg.to_s }.to_json)
   end
 
